215127849688e64b91f68ed13d2e44583091382c
Vinod Santhanam <yes_vinod@yahoo.com>
2021-04-23 20:55:44 -0400

    added rekognition

--- smart-search-api/search_image/app.py
+++ smart-search-api/search_image/app.py
@@ -1,15 +1,32 @@
 import json
-
+import boto3
+import base64
+from datetime import datetime
+import uuid
 # import requests
-
+s3 = boto3.client('s3')
+BUCKET_NAME = 'vin-smart-search'
 
 def lambda_handler(event, context):
+    fileName = event["queryStringParameters"]["fileName"]
+    client=boto3.client('rekognition')
+    response=client.detect_text(Image={'S3Object':{'Bucket':'sam-app-s3uploadbucket-7n3cd8ezfenv','Name':fileName}})
+    print('File Name >> ' + fileName)
+    # now = datetime.now()
   
-
+    # # file_content = base64.b64decode(event['body']['content'])
+    # event_body = json.loads(event['body'])
+    # print('==============================================')
+    # # print('event_body >>' +event_body)
+    # print('==============================================')
+    # print('event_body content >>' +event_body['content'])
+    # print('==============================================')
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world - POST Function",
-            # "location": ip.text.replace("\n", "")
+            "fileName": fileName,
+            "response": response
         }),
     }
+ 
